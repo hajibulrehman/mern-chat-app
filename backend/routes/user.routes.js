9d95847e38c7e40175d8ff83c03ea90f76c36a8e
Hasina <130023461+gkylsk@users.noreply.github.com>
2025-06-26 17:29:14 +0530

    Deployment

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,7 @@ import { getUsersForSidebar } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+// get all users for sidebar
 router.get("/", protectRoute, getUsersForSidebar);
 
 export default router;
